abaad71b5bc311d850c8f05dd91955fc90f1a7ac
anju <20214315.anju@gmail.com>
2024-12-17 19:00:28 +0530

    Initial commit with .gitignore

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hey hey
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
